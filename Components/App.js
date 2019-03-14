9aa33532656803570e659028a99f5cecefa7d9bc
ปรีดาภรณ์ อึ้งนภารัตน์ <s6002041520051@Macmini-213-01.local>
2019-03-14 16:36:28 +0700

    create App Components

---
+++ Components/App.js
@@ -0,0 +1,20 @@
+//import Library
+import React from 'react';
+import { View,Text } from 'react-native';
+
+
+//Writer components
+class App extends React.Component {
+    render(){
+        return(
+            <View>
+                <Text>First</Text>
+            </View>
+        );
+    }
+}
+
+//export
+export default App;
+
+
