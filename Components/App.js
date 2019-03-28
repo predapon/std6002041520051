3e79d76483d7162c14f561b775fd11cc20239e42
ปรีดาภรณ์ อึ้งนภารัตน์ <s6002041520051@Macmini-213-01.local>
2019-03-28 14:53:13 +0700

    decorate component with style

--- Components/App.js
+++ Components/App.js
@@ -1,13 +1,14 @@
 //import Library
 import React from 'react';
 import { View,Text } from 'react-native';
-
+import Header from './Header';
 
 //Writer components
 class App extends React.Component {
     render(){
         return(
             <View>
+                <Header title="Albums" />
                 <Text>Predapon Aungnaparat........</Text>
             </View>
         );
--- Components/Header.js
+++ Components/Header.js
@@ -7,10 +7,25 @@ import {View,Text} from 'react-native';
 class Header extends Component{
     render(){
         return(
-        <View>
-            <Text>{this.props.title}</Text>
+        <View style={styles.header}>
+            <Text style={styles.text} >{this.props.title}</Text>
         </View>
         );
+    }
+}
+//style
+const styles = {
+    header: {
+        padding: 15,
+        backgroundColor:"#F1CE33"
+        
+    },
+    text:{
+        fontSize:20,
+        color:"#000000",
+        textAlign:"center",
+        
+
     }
 }
 //export
